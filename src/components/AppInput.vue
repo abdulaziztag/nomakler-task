ed2e237e43ff0074bc056e095642e1612ca9a022
Abdulaziz <mannopov481@gmail.com>
2023-02-04 00:42:54 +0500

    feat: added reusable input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  defineProps({
+    label: {
+      type: [String, Boolean],
+      default: false
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  })
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <label
+      v-if="label"
+      class="text-sm text-[#506277] my-2"
+    >{{ label }}</label>
+    <input
+      type="text"
+      :value="modelValue"
+      class="rounded-md outline-0 px-3 py-2 items-center focus:border-blue-500 focus:border-2 border-2 border-white"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+  </div>
+</template>
+
+<style scoped>
+
+</style>
