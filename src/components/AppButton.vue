025d58060fc5d501690de6c025fb4b5e4c3e1063
Abdulaziz <mannopov481@gmail.com>
2023-02-04 00:43:07 +0500

    feat: added reusable button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {BUTTON_TYPE} from '../interfaces/Button';
+import {toRefs} from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  buttonType: {
+    type: String,
+    default: 'active'
+  },
+  to: {
+    type: String,
+    default: ''
+  }
+})
+
+const {to} = toRefs(props)
+const goTo = () => {
+  to.value.length !== 0 && router.push(to.value)
+}
+
+</script>
+
+<template>
+  <button
+    :disabled="disabled"
+    :class="['text-center w-full py-3 border-2 focus:border-2 rounded-xl font-bold', BUTTON_TYPE[buttonType]]"
+    @click="goTo"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/interfaces/Button.ts
@@ -0,0 +1,5 @@
+export enum BUTTON_TYPE {
+	disabled = 'text-white bg-gray-500',
+	active = 'text-white bg-blue-500 focus:border-blue-300',
+	light = 'text-blue-700 bg-blue-100 focus:border-blue-500'
+}
